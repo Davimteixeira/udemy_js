3187fb70bf559608f5d76c93dfce7572484b9519
Davimteixeira <daviteixeira077@gmail.com>
2024-07-25 18:26:41 -0300

    começando js

---
+++ 2_OPERADORES/JS/scripts.js
@@ -0,0 +1,12 @@
+// 1 - Number 
+console.log(typeof 2);
+console.log(typeof -127);
+console.log(typeof 5.14);
+
+// operaçãoes aritméticas
+console.log(2 + 4)
+console.log(10 - 5)
+console.log(2 * 4)
+console.log(10 / 2)
+
+console.log(2 + (8*2))
