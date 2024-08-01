104cc76b9215e74adef833212b2deb3258d55492
Davimteixeira <daviteixeira077@gmail.com>
2024-08-01 10:26:29 -0300

    propiedades

--- 5_ARRAYS_E_BJS/js/scripts.js
+++ 5_ARRAYS_E_BJS/js/scripts.js
@@ -2,8 +2,25 @@
 const lista = [1, 2, 3, 4, 5];
 
 console.log(lista);
-console.log(typeof lista)
+console.log(typeof lista);
 
-const itens = ["davi", true, 2, 3.23, []]
+const itens = ["davi", true, 2, 3.23, []];
 
-console.log(itens)
+console.log(itens);
+
+// 2 - mais sobre arrays
+const arr = ["a", "b", "c", "d", "d"];
+
+console.log(arr[0]);
+console.log(arr[2]);
+
+// 3 - Propiedades
+
+const numbers = [1, 2, 3];
+
+console.log(numbers.length);
+console.log(numbers['length']);
+
+const myName = "David";
+console.log(myName.length)
+
