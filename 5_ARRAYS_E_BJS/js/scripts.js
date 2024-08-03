757d1fa2750d4e9cf636e847444381a53180d086
Davimteixeira <daviteixeira077@gmail.com>
2024-08-03 14:54:17 -0300

    foreach

--- 5_ARRAYS_E_BJS/js/scripts.js
+++ 5_ARRAYS_E_BJS/js/scripts.js
@@ -187,3 +187,26 @@ console.log(subArray2);
 
 const subArray3 = testeSlice.slice(10, 20);
 console.log(subArray3);
+
+// 2 em diante
+const subArray4 = testeSlice.slice(2);
+console.log(subArray4);
+
+// 15 - foreach
+const nums = [1, 2, 3, 4, 5];
+
+nums.forEach((numero) => {
+    console.log(`O Número é ${numero}`)
+})
+
+const posts = [
+    { title: "Primeiro post", category: "PHP" },
+    { title: "Segundo post", category: "JavaScript" },
+    { title: "Terceiro post", category: "Python" },
+];
+
+
+posts.forEach((post) => {
+    console.log(`Exibindo post: ${post.title}, da categoria: ${post.category}`);
+});
+  
