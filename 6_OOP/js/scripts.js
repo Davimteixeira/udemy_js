1db94f31296112f42c4cdf3063dfeab4a8898e0f
Davimteixeira <daviteixeira077@gmail.com>
2024-08-05 13:39:56 -0300

    aprofundando em métodos

--- 6_OOP/js/scripts.js
+++ 6_OOP/js/scripts.js
@@ -9,3 +9,25 @@ const animal = {
 console.log(animal.nome)
 
 animal.latir()
+
+
+// 2 - aprofundando em métodos
+const pessoa = {
+    nome: "Matheus",
+  
+    getNome: function () {
+      return this.nome;
+    },
+  
+    setNome: function (novoNome) {
+      this.nome = novoNome;
+    },
+  };
+  
+  console.log(pessoa.nome);
+  
+  console.log(pessoa.getNome());
+  
+  pessoa.setNome("Matheus Battisti");
+  
+  console.log(pessoa.getNome());
