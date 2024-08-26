93f41b6348826e72d65f7dbea430df9af91c334f
Davimteixeira <daviteixeira077@gmail.com>
2024-08-26 14:55:13 -0300

    primeiro request com axios

--- 11_JS_MODERNO/js/scripts.js
+++ 11_JS_MODERNO/js/scripts.js
@@ -172,3 +172,28 @@ class Product {
 const shirt = new Product("Camisa gola V", 20);
 
 console.log(shirt.productWithDiscount(10));
+
+// 9 heranca
+class ProductWithAttributes extends Product {
+  constructor(name, price, colors) {
+    super(name, price);
+    this.colors = colors;
+  }
+
+  showColors() {
+    console.log("As cores são: ");
+    this.colors.forEach((color) => {
+      console.log(color);
+    });
+  }
+}
+
+const hat = new ProductWithAttributes("Chapéu", 29.99, [
+  "Preto",
+  "Amarelo",
+  "Verde",
+]);
+
+console.log(hat.name);
+
+hat.showColors();
