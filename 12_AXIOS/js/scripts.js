26d99cd1016edf76f947a4ecb345860dcd7b150a
Davimteixeira <daviteixeira077@gmail.com>
2024-08-26 16:16:57 -0300

    terminando seção de axios

--- 12_AXIOS/js/scripts.js
+++ 12_AXIOS/js/scripts.js
@@ -4,8 +4,8 @@ console.log(axios);
 // 2 - primeiro request
 const getData = async () => {
     try {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/users",
+      const response = await postFetch.get(
+        "/users",
         //   4 - definindo headers
         //   Ver request headers
         {
@@ -24,4 +24,50 @@ const getData = async () => {
     }
   };
   
-  getData();
+getData();
+
+// 3 - imprimir dados na tela
+const container = document.querySelector("#user-container");
+
+const printData = async () => {
+  const data = await getData();
+
+  console.log(data);
+
+  data.forEach((user) => {
+    const div = document.createElement("div");
+
+    const nameElement = document.createElement("h2");
+
+    nameElement.textContent = user.name;
+
+    div.appendChild(nameElement);
+
+    const emailElement = document.createElement("p");
+
+    emailElement.textContent = user.email;
+
+    div.appendChild(emailElement);
+
+    container.appendChild(div);
+  });
+};
+
+printData();
+
+// 5 - post
+const form = document.querySelector("#post-form");
+const titleInput = document.querySelector("#title");
+const bodyInput = document.querySelector("#body");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  postFetch.post("/posts", {
+    body: JSON.stringify({
+      title: titleInput.value,
+      body: bodyInput.value,
+      userId: 1,
+    }),
+  });
+});
