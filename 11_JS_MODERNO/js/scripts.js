bcd02495016508e360cda0b786fdc719e756f4ac
Davimteixeira <daviteixeira077@gmail.com>
2024-08-26 10:40:10 -0300

    arrow function

--- 11_JS_MODERNO/js/scripts.js
+++ 11_JS_MODERNO/js/scripts.js
@@ -27,3 +27,43 @@ function logName() {
 const name = "Matheus";
 
 logName();
+
+// 2 arrow function
+const sum = function (a, b) {
+  return a + b;
+};
+
+const arrowSum = (a, b) => a + b;
+
+console.log(sum(5, 5));
+console.log(arrowSum(5, 5));
+
+const greeting = (name) => {
+  if (name) {
+    return "Hello " + name + "!";
+  } else {
+    return "Hello!";
+  }
+};
+
+console.log(greeting());
+console.log(greeting("Matheus"));
+
+const user = {
+  name: "Theo",
+  sayUserName() {
+    setTimeout(function () {
+      console.log(this);
+      console.log("Username: " + this.name);
+    }, 1000);
+  },
+  sayUserNameArrow() {
+    setTimeout(() => {
+      console.log(this);
+      console.log("Username: " + this.name);
+    }, 1200);
+  },
+};
+
+// user.sayUserName()
+// user.sayUserNameArrow()
